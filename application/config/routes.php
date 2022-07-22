c7749ef390e550196011e20eb3c0e2cf7a74d8ff
GBMU <16015829+agetoz@users.noreply.github.com>
2022-07-22 13:29:26 +0700

    Delete application directory

--- application/config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$route['default_controller'] 	= "adm";
-$route['404_override'] 			= '';
-
-
-/* End of file routes.php */
-/* Location: ./application/config/routes.php */
--- application/views/v_selesai_ujian.php
+++
@@ -1,7 +0,0 @@
-<div class="panel panel-info">
-		<div class="panel-heading">Selesai ujian   </div>
-		<div class="panel-body">
-		<?php echo $data; ?>
-		<!--<a href="<?php echo site_url(); ?>/adm/ikuti_ujian">Kembali</a>-->
-		</div>
-	</div>
